8204ef1079d89728bf528ed2c547a6103553d014
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-05-09 14:10:35 -0500

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MaterialList from "./components/tickets/MaterialList";
+import MaterialDetails from "./components/tickets/MaterialDetails";
+import CreateMaterial from "./components/tickets/CreateMaterial";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="materials">
+          <Route index element={<MaterialList />} />
+          <Route path=":id" element={<MaterialDetails />} />
+          <Route path="create" element={<CreateMaterial />} />
+        </Route>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+);
