86b87340d1a412f6994b69b9045549e3476b3a75
Zachary Trouy <ztrouy@gmail.com>
2024-05-09 16:13:59 -0500

    Added fetch to get a single Patron and all their Checkouts by Id

--- src/data/patronsData.js
+++ src/data/patronsData.js
@@ -3,3 +3,7 @@ const _apiUrl = "/api/patrons";
 export const getPatrons = () => {
     return fetch(_apiUrl).then(res => res.json());
 };
+
+export const getPatron = (id) => {
+    return fetch(`${_apiUrl}/${id}`).then(res => res.json());
+};
