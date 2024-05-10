067048725fb842aab03339f13a1d247b103f7def
Zachary Trouy <ztrouy@gmail.com>
2024-05-10 18:48:07 -0500

    Added fetch to update a Patron

--- src/data/patronsData.js
+++ src/data/patronsData.js
@@ -7,3 +7,15 @@ export const getPatrons = () => {
 export const getPatron = (id) => {
     return fetch(`${_apiUrl}/${id}`).then(res => res.json());
 };
+
+export const updatePatron = (patronUpdate) => {
+    const putOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(patronUpdate)
+    };
+
+    return fetch(`${_apiUrl}/${patronUpdate.id}`, putOptions).then(res => res.json());
+};
