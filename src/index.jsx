370f3c8ff41c506666c353b00393180e3d8b590d
Zachary Trouy <ztrouy@gmail.com>
2024-05-13 12:36:51 -0500

    Added routing for Browse view and Material Checkout form view

--- src/index.jsx
+++ src/index.jsx
@@ -10,6 +10,8 @@ import PatronList from "./components/tickets/PatronList";
 import PatronDetails from "./components/tickets/PatronDetails";
 import PatronEdit from "./components/forms/PatronEdit";
 import CheckoutList from "./components/tickets/CheckoutList";
+import BrowseList from "./components/tickets/BrowseList";
+import MaterialCheckout from "./components/forms/MaterialCheckout";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -18,7 +20,11 @@ root.render(
       <Route path="/" element={<App />}>
         <Route path="materials">
           <Route index element={<MaterialList />} />
-          <Route path=":id" element={<MaterialDetails />} />
+          <Route path="browse" element={<BrowseList />} />
+          <Route path=":id">
+            <Route index element={<MaterialDetails />} />
+            <Route path="checkout" element={<MaterialCheckout />} />
+          </Route> 
           <Route path="create" element={<CreateMaterial />} />
         </Route>
         <Route path="patrons">
